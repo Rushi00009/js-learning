ebc767edbb06f3f348c1decf75d23098982dbaed
Rushikesh <rushikeshpol51@gmail.com>
2024-03-19 13:01:47 +0530

    for loop

---
+++ 06_forLoop.js
@@ -0,0 +1,28 @@
+for (let index = 0; index <= 10; index++) {
+    console.log(index);
+
+}
+
+for (let index = 15; index <= 24; index++) {
+    console.log(index);
+
+}
+// print 10 to 0 
+for (let index = 10; index >= 0; index--) {
+    console.log(index);
+
+}
+console.log(`==========`);
+
+// print Even number  
+for (let index = 0; index <= 20; index += 2) {
+    console.log(index);
+
+}
+
+// print odd number 
+
+for (let index = 1; index <= 20; index += 2) {
+    console.log(index);
+
+}
---
+++ 06_forLoopAssign.js
@@ -0,0 +1,44 @@
+console.log(`Print the number from 5 to 15 by incrementing 1 `);
+
+for (let index = 5; index <= 15; index++) {
+    console.log(index);
+
+}
+
+console.log(`Print the number from 50 to 40 by decrementing 1 `);
+
+for (let index = 50; index >= 40; index--) {
+    console.log(index);
+}
+console.log(`Print the first 15 odd number`);
+
+for (let index = 1; index <= 30; index = index + 2) {
+    console.log(index);
+}
+
+console.log(`Print the first 10 even number`);
+
+for (let index = 2; index <= 20; index = index + 2) {
+    console.log(index);
+
+}
+console.log(`Print the number from 5 to 50 by incrementing 5 `);
+
+for (let index = 5; index <= 50; index = index + 5) {
+    console.log(index);
+}
+
+console.log(`Print the number from 10 to 100 by incrementing 10 `);
+
+
+for (let index = 10; index <= 100; index = index + 10) {
+    console.log(index);
+}
+
+console.log(`Print the number from 100 to 10 by decrementing 10 `);
+
+
+for (let index = 100; index >= 10; index = index - 10) {
+    console.log(index);
+
+}
